2b20fca070d3c246397c3597c738165735daed12
Penny <88145246+nnype@users.noreply.github.com>
2022-03-15 10:57:51 +0200

    first_commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
-const progressBarPercents = [90, 86, 72, 80, 27];
+const progressBarPercents = [90, 86, 72, 80, 67];
 
 window.addEventListener("scroll", () => {
   mainFn();
